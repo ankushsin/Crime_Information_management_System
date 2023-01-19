593362a15a449da42b5a87f58ae98f2915e2adb2
Ankush Singh <101393390+ankushsin@users.noreply.github.com>
2023-01-20 01:05:18 +0530

    Add files via upload

---
+++ Crime_Information_Management_System/src/com/crime_IMS/Utility/DButil.java
@@ -0,0 +1,35 @@
+package com.crime_IMS.Utility;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DButil {
+public static Connection provideConnection() {
+		
+		Connection conn = null;
+		
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		String url = "jdbc:mysql://localhost:3306/tms";
+		
+		 try {
+			conn = DriverManager.getConnection(url, "root","1440");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		 
+		 return conn;
+		
+	
+		
+	}
+
+}
